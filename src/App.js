1811932e171c028ed18a5ceba9d3d2cd735c92e7
Saish SSK <saish.sk@outlook.com>
2018-11-01 19:13:36 +0530

    Added boilerplate for Auth feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component }from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Card, Input } from "react-native-elements";
 
 export default class App extends Component {
     render() {
         return(
             <View style = {styles.appContainer}>
-                <Text>Hellow World !</Text>
+                <Card>
+                    <Text>Hello World !</Text>
+                </Card>
             </View>
         );
     }
